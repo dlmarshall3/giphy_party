3b98cc3cbb46461b0f7e2cb4b46c281dfca61978
David Marshall <dlmarshall3@protonmail.com>
2021-11-12 07:31:34 -0600

    uploading for submission

---
+++ app.js
@@ -0,0 +1,34 @@
+const token = "Rl5sN8oj6HlOtisslGhhMPCMJPvvtUAZ"
+const searchBar = document.querySelector('#searchBar');
+const submitBtn = document.querySelector('#submitBtn')
+const form = document.querySelector('#gifForm')
+const gifCanvas = document.querySelector('#gifCanvas')
+
+const baleeted = document.querySelector('#delete')
+
+
+
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
+        params: {
+            q: searchBar.value,
+            api_key: token
+        }
+    });
+
+    let giphyArr = res.data.data.length;
+    let randomIndex = Math.floor(Math.random() * giphyArr)
+    let gifImg = document.createElement('img');
+    gifImg.src = res.data.data[`${randomIndex}`].images.original.url;
+    gifImg.classList.add('createdGif')
+    gifCanvas.prepend(gifImg)
+    searchBar.value = "";
+})
+
+baleeted.addEventListener('click', function(e){
+    let player = document.querySelector('#homestar');
+    player.play();
+    gifCanvas.innerHTML = '';
+})
